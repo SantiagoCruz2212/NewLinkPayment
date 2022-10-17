190c54cfd77ad3cd58c9aafeb9b4cbfd97356920
SantiagoCruz2212 <70975033+SantiagoCruz2212@users.noreply.github.com>
2022-10-17 15:45:03 -0500

    Last commit

---
+++ Frontend/src/app/public-modules/components/confirm-purchase-view/confirm-purchase-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'confirm-purchase-view',
+  templateUrl: './views/confirm-purchase-view.component.html',
+  styleUrls: ['./styles/confirm-purchase-view.component.scss']
+})
+export class ConfirmPurchaseViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Frontend/src/app/public-modules/components/purchase-tickets-view/purchase-tickets-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'purchase-tickets-view',
+  templateUrl: './views/purchase-tickets-view.component.html',
+  styleUrls: ['./styles/purchase-tickets-view.component.scss']
+})
+export class PurchaseTicketsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
